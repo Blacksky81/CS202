1bd97fc4381f54451406cb7da93e73ed823259b8
Joohyun Kim <bbaggoome@gmail.com>
2019-02-08 19:54:40 -0800

    Quiz 4

---
+++ main.js
@@ -0,0 +1,65 @@
+// 
+// // comments
+// /* mutiple lines comments */
+
+// //statements
+// //window.alert('Hello World')
+
+// //print output information
+// window. alert('Enter Your Name')
+
+// document.write('Write is your email');
+
+// prompt('Enter Your Email');
+// coonfirm('Do you agree?');
+// console.log('In console now')
+// parseInt(prompt('Enter Your Age'));
+//elementinnerHTML;
+
+// //variables - storage location
+
+// var variablename = 'Julyien';
+// var text = 'Year old';
+// alert(text);
+// document.write(variablename);
+// //concatenation +
+// document.write('<h2> Hello ' + variablename + '</h2>');
+
+// var date = Date();
+// document.write(date);
+
+// var userColor = prompt('Enter your favorite color?');
+
+// document.bgColor = userColor;
+
+
+//Date types
+//Numbers, String, Boolenas, Objects, Arrays
+//Integer, float,..
+
+var num = 2333;
+var txt = 'Julyien';
+var gaveOver = true/false;
+var student =(
+    Name:"Julyien",
+    Age:"25",
+    eyeColor:"green",
+)
+
+var colors = ('green', 'red', 'black');
+
+//function - blocks of codes that are used for a specific purpose.
+
+function func(){
+    //run your funtion codes
+    alert('Hi New User');
+}
+//call the function
+func();
+
+func doSomething(){
+    document.write();
+}
+
+//Event - actions of something that the browser or user reacts to.
+//click, load, resize, mouseiver, blur, change, mouseout,  
